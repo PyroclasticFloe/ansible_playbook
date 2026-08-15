db9e3e46b5b94ce124180e3739b4aaf8a763295b
aria <aria@containers>
2026-08-15 10:40:13 -0400

    fix helper vault script

--- edit_vault.sh
+++ edit_vault.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-ansible-vault edit ~/containers/ansible/group_vars/all/vault.yml \
-    --vault-password-file ~/private/ansible.vault
+ansible-vault edit ~/containers/ansible/group_vars/all/vault.yml --vault-password-file ~/private/ansible.vault
 
