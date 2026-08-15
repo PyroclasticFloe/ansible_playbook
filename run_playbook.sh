2e91626a1685dd6a7d7049ad98c96f582152d77c
aria <aria@containers>
2026-08-15 10:39:56 -0400

    fix helper scripts

--- run_playbook.sh
+++ run_playbook.sh
@@ -1,5 +1,4 @@
 #/usr/bin/env bash
 cd ~/containers/ansible
-ansible-playbook -i inventory ./playbook.yml /
-  --vault-password-file ~/private/ansible.vault
+ansible-playbook -i inventory ./playbook.yml --vault-password-file ~/private/ansible.vault
 cd -
