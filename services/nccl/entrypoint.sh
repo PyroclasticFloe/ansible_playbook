0eb89f0ca4326cc76212e546b45678db8a099935
aria <aria@containers>
2026-09-12 13:10:48 -0400

    update entrypoint.sh to make nccl ssh access work.

--- services/nccl/entrypoint.sh
+++ services/nccl/entrypoint.sh
@@ -10,7 +10,7 @@ if [ ! -f /etc/ssh/ssh_host_ed25519_key ]; then
     ssh-keygen -A
 fi
 
-if [ -f /etc/ssh/authorized_keys/app ]; then
+if [ -f /etc/ssh/authorized_keys/$USER_NAME ]; then
     install -d -m 700 \
         -o "${USER_UID}" \
         -g "${USER_UID}" \
@@ -19,7 +19,7 @@ if [ -f /etc/ssh/authorized_keys/app ]; then
     install -m 600 \
         -o "${USER_UID}" \
         -g "${USER_UID}" \
-        /etc/ssh/authorized_keys/app \
+        /etc/ssh/authorized_keys/$USER_NAME \
         "${USER_HOME}/.ssh/authorized_keys"
 fi
 
@@ -28,6 +28,12 @@ chown "${USER_UID}:${USER_UID}" "${USER_HOME}"
 if [ -d "${USER_HOME}/.ssh" ]; then
     chown "${USER_UID}:${USER_UID}" "${USER_HOME}/.ssh"
     chmod 700 "${USER_HOME}/.ssh"
+fi
+
+if passwd -S ubuntu | awk '{print $2}' | grep -q '^L$'; then
+    RANDOM_PASSWORD="$(head -c 48 /dev/urandom | base64 -w0)"
+    PASSWORD_HASH="$(openssl passwd -6 "$RANDOM_PASSWORD")"
+    usermod -p "$PASSWORD_HASH" ubuntu
 fi
 
 /usr/sbin/sshd -t
